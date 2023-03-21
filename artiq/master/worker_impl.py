64df7e074e671668896a1f3a6e93c32e9a4d07d6
jpagett <pagett.jared@gmail.com>
2023-03-21 08:43:19 -0700

    remove hour folder from worker_impl.py dataset saving

--- artiq/master/worker_impl.py
+++ artiq/master/worker_impl.py
@@ -327,8 +327,7 @@ def main():
                     rid, obj["pipeline_name"], expid, obj["priority"])
                 start_local_time = time.localtime(start_time)
                 dirname = os.path.join("results",
-                                   time.strftime("%Y-%m-%d", start_local_time),
-                                   time.strftime("%H", start_local_time))
+                                   time.strftime("%Y-%m-%d", start_local_time))
                 os.makedirs(dirname, exist_ok=True)
                 os.chdir(dirname)
                 argument_mgr = ProcessArgumentManager(expid["arguments"])
